e5f0c700292519e68f6d6eb8b07c3041ba983b33
David Sillman <dsillman2000@gmail.com>
2026-02-09 08:32:49 -0500

    feat: drastically simplify overall implementation to just the __init__ file and CLI.

--- tests/unit/test_reference.py
+++ tests/unit/test_reference.py
@@ -1,23 +1,28 @@
-from yaml_reference import YAMLReference
+from pathlib import Path
+
+from yaml_reference import (
+    Reference,
+    ReferenceAll,
+    load_yaml_with_references,
+    parse_yaml_with_references,
+)
 
 
 def test_reference_load(stage_files):
-
-    yaml = YAMLReference()
 
     files = {
         "test.yml": "hello: world\ncontents: !reference { path: ./inner.yml }",
         "inner.yml": "inner: inner_value\ninner_list:\n  - inner_list_value_1\n  - inner_list_value_2",
     }
     stg = stage_files(files)
-    data = yaml.load(stg / "test.yml")
+    data = load_yaml_with_references(stg / "test.yml")
     assert data["hello"] == "world"
     assert data["contents"]["inner"] == "inner_value"
 
     files["inner.yml"] += "\nanother_inner: !reference { path: deeper/inner2.yml }"
     files["deeper/inner2.yml"] = "inner2: inner2_value"
     stg = stage_files(files)
-    data = yaml.load(stg / "test.yml")
+    data = load_yaml_with_references(stg / "test.yml")
     assert data["hello"] == "world"
     assert data["contents"]["inner"] == "inner_value"
     assert data["contents"]["another_inner"]["inner2"] == "inner2_value"
@@ -27,7 +32,7 @@ def test_reference_load(stage_files):
     )
     files["deeper/yet/inner3.yml"] = "inner3: inner3_value"
     stg = stage_files(files)
-    data = yaml.load(stg / "test.yml")
+    data = load_yaml_with_references(stg / "test.yml")
     assert (
         data["contents"]["another_inner"]["another_inner2"]["inner3"] == "inner3_value"
     )
@@ -35,13 +40,11 @@ def test_reference_load(stage_files):
     files["leaf.yml"] = "leaf_value\n..."
     files["deeper/yet/inner3.yml"] += "\nleaf: !reference { path: ../../leaf.yml }"
     stg = stage_files(files)
-    data = yaml.load(stg / "test.yml")
+    data = load_yaml_with_references(stg / "test.yml")
     assert data["contents"]["another_inner"]["another_inner2"]["leaf"] == "leaf_value"
 
 
 def test_reference_all_load(stage_files):
-    yaml = YAMLReference()
-
     files = {
         "test.yml": "hello: world\ncontents: !reference-all { glob: ./chapters/*.yml }",
         "chapters/chapter1.yml": "chapter_value: 1\n",
@@ -49,7 +52,7 @@ def test_reference_all_load(stage_files):
         "chapters/chapter3.yml": "chapter_value: 3\n",
     }
     stg = stage_files(files)
-    data = yaml.load(stg / "test.yml")
+    data = load_yaml_with_references(stg / "test.yml")
     assert data
     assert data["hello"] == "world"
     assert len(data["contents"]) == 3
@@ -64,8 +67,34 @@ def test_reference_all_load(stage_files):
         "chapters/chapter2/summary.yml": "chapter2_summary: 2\n",
     }
     stg = stage_files(files)
-    data = yaml.load(stg / "test.yml")
+    data = load_yaml_with_references(stg / "test.yml")
     assert data
     assert len(data["inner"]["open"]) == 2
     assert {"chapter1_summary": 1} in data["inner"]["open"]
     assert {"chapter2_summary": 2} in data["inner"]["open"]
+
+
+def test_parse_references(stage_files):
+    files = {
+        "test.yml": "inner: !reference { path: next/open.yml }\n",
+        "next/open.yml": "open: !reference-all { glob: ../chapters/*/summary.yml }\n",
+        "chapters/chapter1/summary.yml": "chapter1_summary: 1\n",
+        "chapters/chapter2/summary.yml": "chapter2_summary: 2\n",
+    }
+    stg = stage_files(files)
+    data = parse_yaml_with_references(stg / "test.yml")
+    assert data
+    assert data["inner"] is not None
+    assert isinstance(data["inner"], Reference)
+    assert data["inner"].path == "next/open.yml"
+    assert hasattr(data["inner"], "location")
+    assert data["inner"].location == str((stg / "test.yml").absolute())
+
+    ref_loc = Path(data["inner"].location)
+    ref_path = data["inner"].path
+    data = parse_yaml_with_references(ref_loc.parent / ref_path)
+    assert data
+    assert data["open"] is not None
+    assert isinstance(data["open"], ReferenceAll)
+    assert data["open"].glob == "../chapters/*/summary.yml"
+    assert data["open"].location == str((stg / "next/open.yml").absolute())
